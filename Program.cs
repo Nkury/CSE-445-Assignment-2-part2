b1046869e3fb547994008d00b2656f5789c7e154
Nizar Kury <nizark95@yahoo.com>
2015-09-25 22:47:07 -0700

    main comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Programmer:  Nizar Kury
+// Instructor:  Professor Chen
+// Date:        9/25/2015
+// Description: The Main Class creates and starts all the airline and travel agency threads, instantiates global buffers and semaphores, and
+//              begins the program.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +15,17 @@ namespace Assignment2
 {
     class Program
     {
-         public static AutoResetEvent orderCompleted = new AutoResetEvent(false); // when the airline is done processing the order, this event
-                                                                          // coordinates with the travel agency so it can print the timestamp
-         public static MultiCellBuffer bufferRef = new MultiCellBuffer(); // instantiate buffer that will hold the orders of travel agencies
-         public static confirmBuffer confirm = new confirmBuffer();
+         public static MultiCellBuffer bufferRef = new MultiCellBuffer(); // instantiate multicell buffer that will hold the orders of travel agencies
+         public static confirmBuffer confirm = new confirmBuffer();     // instantiate the confirmation buffer that will alert travel agencies
+                                                                        // that their order has been successfully processed
          public static Semaphore semaphore = new Semaphore(0, 3);   // semaphore for keeping track of how many cells are open
-         public static ReaderWriterLock rwlock = new ReaderWriterLock();
+      
         static void Main(string[] args)
         {
-            Program.semaphore.Release(3);
-            // change
-            Airline air = new Airline(); // create an airline to pass the airline function for threading
+            Program.semaphore.Release(3); // sets the semaphore to the max value of the multicell buffer size, which is 3
+
+            // create 3 airline objects to pass the airline function for threading 
+            Airline air = new Airline(); 
             Airline air2 = new Airline();
             Airline air3 = new Airline();
 
@@ -27,24 +33,25 @@ namespace Assignment2
             Thread airline1 = new Thread(new ThreadStart(() => air.priceModel("airline1"))); // start the thread with the passed-in parameter airline1 string
             Thread airline2 = new Thread(new ThreadStart(() => air2.priceModel("airline2"))); // start the thread with the passed-in parameter airline2 string
             Thread airline3 = new Thread(new ThreadStart(() => air3.priceModel("airline3"))); // start the thread with the passed-in parameter airline3 string
-            airline1.Name = "airline 1";
-            airline2.Name = "airline 2";
-            airline3.Name = "airline 3";
-            airline1.Start();
-            airline2.Start();
-            airline3.Start();
+            airline1.Name = "airline 1";    // give the thread a unique name
+            airline2.Name = "airline 2";    // give the thread a unique name
+            airline3.Name = "airline 3";    // give the thread a unique name
+            airline1.Start();   // start the thread
+            airline2.Start();   // start the thread
+            airline3.Start();   // start the thread
             
             // Instantiate a travel agency object in order to use its methods
             TravelAgency travel = new TravelAgency();
             Airline.priceCut += new priceCutEvent(travel.ticketsOnSale);       // have the travel agency method subscribe to airline event
 
             Thread[] travelAgencies = new Thread[5]; // create 5 travel agency threads
+
             for (int i = 0; i <= 4; i++) // N = 5 (for five travel agency threads)
             {
                 // Start 5 travel agency threads
-                travelAgencies[i] = new Thread(new ThreadStart(travel.travelAgencyFunc));
-                travelAgencies[i].Name = "travel agency " + (i+1).ToString();
-                travelAgencies[i].Start();
+                travelAgencies[i] = new Thread(new ThreadStart(travel.travelAgencyFunc));   // starts the thread with the travelAgencyFunc method
+                travelAgencies[i].Name = "travel agency " + (i+1).ToString();   // gives each thread a unique name
+                travelAgencies[i].Start();  // starts the thread
             }
 
         }
